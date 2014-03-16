9bbb01c10cbd63c3b7a2b4e19b2aa4cc440efd76
Avijeet Sachdev <info@avijeets.com>
2014-03-15 21:04:34 -0400

    added bootstrap sass

--- README.md
+++
@@ -1,5 +0,0 @@
-# PinterestBoards
-
-Simple Rails project that aims to clone Pinterest's concept.
-
-by [Avijeet Sachdev](http://avijeets.com/)
--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Pinteresting::Application.config.secret_key_base = '8dddee01fe9ee148770d75c9714f2782227904bb77b7e59633012c7d81857cefc86815cdb78c4e55380c54babdc7fe13861ad2d8d3619ab6b22bfb81a663ab04'
+Pinteresting::Application.config.secret_key_base = 'c14c4439879c19ff899da57788ba255dcc26cb04626e79d784e19f3c4e1cb1e404e489349f48e34bfaf20786c57d768f5a0c8ec94577e54e9024f778908acff4'
